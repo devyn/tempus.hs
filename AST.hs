1047cee4c4e1981b8cdf2ffe1598d02d915a66f5
Micah Johnston <wyndclaw@gmail.com>
2012-08-14 09:28:20 -0600

    refining the AST

--- AST.hs
+++ AST.hs
@@ -1,6 +1,7 @@
 module AST where
 
-data Definition = Definition [String] [String] [Expression]
+data Definition = Function String [String] [Expression]
+                | Value   [String]         [Expression]
 data Expression = Reference String [Expression]
                 | Object [Definition]
                 | Conditional [(Expression, Expression)]
