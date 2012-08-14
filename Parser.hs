b0ff1065e9e1a22a7e82621bd3b4f4f59ce3406b
Micah Johnston <wyndclaw@gmail.com>
2012-08-14 11:53:50 -0600

    we now parse everything except infix operators

---
+++ Parser.hs
@@ -0,0 +1,26 @@
+module Parser where
+import Text.ParserCombinators.Parsec hiding (many, optional, (<|>))
+import Control.Applicative
+import AST
+
+scope :: Parser [Definition] 
+scope = many (definition <* optional (lexeme ";"))
+
+definition :: Parser Definition
+definition = try (Function <$> name <* lexeme "("
+                           <*> sepBy1 name (try (lexeme ",")) <* lexeme ")" <* lexeme "="
+                           <*> sepBy1 expression (lexeme ","))
+             <|> (Value    <$> sepBy1 name (lexeme ",") <* lexeme "="
+                           <*> sepBy1 expression (lexeme ","))
+
+expression :: Parser Expression
+expression = try (Object <$> (lexeme "{" *> scope <* lexeme "}"))
+         <|> try (Call   <$> name <* lexeme "("
+                         <*> sepBy1 expression (lexeme ","))
+         <|>     (Name   <$> name)
+
+name :: Parser String
+name = spaces *> many1 (noneOf " \n\t{}(),;") <* spaces
+
+lexeme :: String -> Parser String
+lexeme s = spaces *> string s <* spaces
