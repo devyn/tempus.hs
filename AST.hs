0b1b691eeb302ba395a9f8f57352a360ebb2b707
Micah Johnston <wyndclaw@gmail.com>
2012-08-14 16:41:50 -0600

    add looking up members to the AST

--- AST.hs
+++ AST.hs
@@ -1,7 +1,9 @@
 module AST where
 
 data Definition = Function String [String] [Expression]
-                | Value   [String]         [Expression]
-data Expression = Reference String [Expression]
+                | Value   [String]         [Expression]  deriving (Show)
+data Expression = Name String
+                | Lookup Expression String
+                | Call String [Expression]
                 | Object [Definition]
-                | Conditional [(Expression, Expression)]
+                | Conditional [(Expression, Expression)] deriving (Show)
