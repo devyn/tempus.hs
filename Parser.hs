503a0c4d96b62766f1db4f08e7fe28dc97e871c5
Micah Johnston <wyndclaw@gmail.com>
2012-08-15 09:48:41 -0600

    add support to accessing members to the parser

--- Parser.hs
+++ Parser.hs
@@ -10,20 +10,41 @@ scope :: Parser [Definition]
 scope = many (definition <* optional (lexeme ";"))
 
 definition :: Parser Definition
-definition = try (Function <$> name <* lexeme "("
-                           <*> sepBy1 name (try (lexeme ",")) <* lexeme ")" <* lexeme "="
+definition = try (Function <$> name
+                           <*> bracket "(" ")" (sepBy1 name (try (lexeme ","))) <* lexeme "="
                            <*> sepBy1 expression (lexeme ","))
-         <|>     (Value    <$> sepBy1 name (lexeme ",") <* lexeme "="
+             <|> (Value    <$> sepBy1 name (lexeme ",") <* lexeme "="
                            <*> sepBy1 expression (lexeme ","))
 
 expression :: Parser Expression
-expression = try (Object <$> (lexeme "{" *> scope <* lexeme "}"))
-         <|> try (Call   <$> name <* lexeme "("
-                         <*> sepBy1 expression (lexeme ","))
-         <|>     (Name   <$> name)
+expression = term
+
+term :: Parser Expression
+term = try (foldl (uncurry . Member) <$> value <*> many (lexeme "." *> reference))
+       <|> (Reference <$> choice (map lexeme prefixes) <*> ((:[]) <$> term))
+
+value :: Parser Expression
+value = try (bracket "(" ")" expression)
+    <|> try (Object <$> bracket "{" "}" scope)
+    <|> (uncurry Reference <$> reference)
+
+reference :: Parser (String, [Expression])
+reference = (,) <$> name
+                <*> (try (bracket "(" ")" (sepBy1 expression (lexeme ","))
+                     <|> return []))
 
 name :: Parser String
-name = spaces *> many1 (noneOf " \n\t{}(),;") <* spaces
+name = spaces *> many1 (noneOf " \n\t{}(),;.") <* spaces
+
+infixes :: [[String]]
+infixes = [ [ "+", "-" ]
+          , [ "*", "/" ] ]
+
+prefixes :: [String]
+prefixes = [ "-" ]
 
 lexeme :: String -> Parser String
 lexeme s = spaces *> string s <* spaces
+
+bracket :: String -> String -> Parser a -> Parser a
+bracket l r p = between (lexeme l) (lexeme r) p
