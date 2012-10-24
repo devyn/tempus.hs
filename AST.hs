13bdbb1e5fbd54ac145ed03121b16c3091bb4cbb
Micah Johnston <wyndclaw@gmail.com>
2012-10-23 18:37:06 -0600

    senseless rearranging

--- AST.hs
+++ AST.hs
@@ -3,7 +3,7 @@ module AST where
 data Definition = Value   [String]         [Expression]
                 | Function String [String] [Expression] deriving (Show)
 data Expression = Reference String [Expression]
-                | Infix String Expression Expression
                 | Member Expression String [Expression]
+                | Infix String Expression Expression
                 | Object [Definition]
                 | Conditional [(Expression, Expression)] deriving (Show)
