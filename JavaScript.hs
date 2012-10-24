ac1acfd515778f8e9514388d7b9fc5f1e9743ee2
Micah Johnston <wyndclaw@gmail.com>
2012-10-24 12:50:48 -0600

    compiling is in a functional but rough state

--- JavaScript.hs
+++ JavaScript.hs
@@ -3,24 +3,46 @@ import Data.List
 import AST
 
 compiler :: [Definition] -> String
-compiler program = intercalate "\n\n" (map definition program)
+compiler program = intercalate "\n" (map definition program)
 
 definition :: Definition -> String
-definition (Value names values) = if length names == 1 && length values == 1
-  then
-    "var " ++ head names ++ " = " ++ expression (head values) ++ ";"
-  else
-    "var " ++ intercalate ", " names ++ ";\n" ++
-    "(function($) {\n" ++ indent
-      (intercalate "\n" (zipWith (++) (map (++ " = $[") names) (map ((++ "];") . show) [0..]))) ++
-    "})([" ++ intercalate ", " (map expression values) ++ "]);"
-definition (Function name arguments values) = name ++ " = "
+definition (Value names values) =
+  "var " ++ intercalate ", " names ++ ";\n" ++
+  "(function($) {\n" ++ indent
+    (concat (zipWith (++) (map (++ " = $[") names) (map ((++ "];\n") . show) [0..]))) ++
+  "})("++ {-(if length values > 1 then "[" else "")-}"[" ++
+    intercalate ", " (map expression values) ++
+  {-(if length values > 1 then "]" else "")-}"]" ++ ");\n"
+definition (Function name arguments values) =
+  "var " ++ name ++ " = function(" ++ intercalate ", " arguments ++ ") {\n" ++ indent
+    ("return " ++
+{-    (if length values == 1
+      then
+        expression (head values) ++ ";"
+      else-}
+        "[" ++ intercalate ", " (map expression values) ++ "];") ++
+  "};"
 
 expression :: Expression -> String
 expression (Reference name []) = name
 expression (Reference name args) = name ++ "(" ++ intercalate ", " (map expression args) ++ ")"
 expression (Member owner name []) = (expression owner) ++ "." ++ name
 expression (Member owner name args) = (expression owner) ++ "." ++ name ++ "(" ++ intercalate ", " (map expression args) ++ ")"
+expression (Infix oper left right) = term left ++ " " ++ oper ++ " " ++ term right where
+expression (Prefix oper expr) = oper ++ term expr
+expression (Object definitions) = let names = concatMap definitionNames definitions in
+  "(function() {\n" ++ indent
+    (compiler definitions) ++
+    "return { " ++ intercalate ", " (zipWith (++) (map (++ ": ") names) names) ++ " }" ++
+  "})()"
+
+term t@(Infix _ _ _) = "(" ++ expression t ++ ")"
+term t@(Conditional _) = "(" ++ expression t ++ ")"
+term t = expression t
+
+definitionNames :: Definition -> [String]
+definitionNames (Value names _) = names
+definitionNames (Function name _ _) = [name]
 
 indent :: String -> String
 indent = unlines . map ("  " ++) . lines
