a3fcd519a8e1cef2b2cbdf7944f9dabc0e5f12cc
Micah Johnston <micahrjohnston@gmail.com>
2012-12-08 00:13:46 -0700

    nicer frontend

--- Main.hs
+++ Main.hs
@@ -6,8 +6,13 @@ import System.Environment
 
 main :: IO ()
 main = do
-    (inFile:outFile:_) <- getArgs
-    program <- readFile inFile
-    case parser inFile program of
+  args <- getArgs
+  if length args < 2
+    then
+      putStrLn "Expect input and output files."
+    else do
+      let (inFile:outFile:_) = args
+      program <- readFile inFile
+      case parser inFile program of
         Left err -> putStrLn err
         Right program -> writeFile outFile (compiler program)
